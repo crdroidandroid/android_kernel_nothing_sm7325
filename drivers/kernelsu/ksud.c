1a03f527e5beab16bf8d4b87c371c3e00068fb8a
MySelly <xusufgul@gmail.com>
2026-03-11 21:02:46 +0000

    Revert "kernelsu: replace renameat hook with fsnotify"
    This reverts commit d46b73b5878f476360a566f56c7180648062b644.

--- drivers/kernelsu/ksud.c
+++ drivers/kernelsu/ksud.c
@@ -1,4 +1,3 @@
-#include "manager.h"
 #include <asm/current.h>
 #include <linux/compat.h>
 #include <linux/cred.h>
@@ -85,7 +84,6 @@ void on_post_fs_data(void)
 	done = true;
 	pr_info("on_post_fs_data!\n");
 	ksu_load_allow_list();
-	ksu_observer_init();
 	// sanity check, this may influence the performance
 	stop_input_hook();
 
